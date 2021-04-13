ed23e30bc833d70876638ef11cb012e9a32d7d29
Lesny <czarek.lesny@gmail.com>
2021-04-13 20:59:55 +0200

    final result

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,18 +1,20 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blogapp.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('Nazwa użytkownika',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    confirm_password = PasswordField('Potwierdź hasło ',
                                      validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Zarejestruj się')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -27,11 +29,31 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    remember = BooleanField('Zapamiętaj mnie')
+    submit = SubmitField('Zaloguj')
 
 class PostForm(FlaskForm):
     title = StringField('Tytuł', validators=[DataRequired()])
     content = TextAreaField('Treść', validators=[DataRequired()])
     submit = SubmitField('Utwórz')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Nazwa użytkownika',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Zaktualizuj zdjęcie profilowe', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Zaktualizuj konto')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Ten login jest już zajęty, proszę wybrać inny.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Ten e-mail jest już w użyciu.')
